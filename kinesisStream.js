770c1272d11659ae325ec59e044f8f7cbc20458c
samwhite <sam.white@trinitymirror.com>
2015-09-17 15:49:42 +0100

    replace recursion with stream feedback

--- kinesisStream.js
+++ kinesisStream.js
@@ -1,103 +1,81 @@
 var aws = require ( 'aws-sdk' );
 var R = require ( 'ramda' );
-var hl = require ( 'highland' );
+var hl = require ( 'highland');
+var K = new aws.Kinesis()
 
 var timeouts = [ 0, 2000 ];
 var debug = false;
 
-var log = function ( message ) {
+var log = function(message) {
     var inspect;
 
-    if ( debug ) {
-        inspect = require ( 'eyes' ).inspector ( { maxLength: 0 } );
-
-        ( {
-            'Array': inspect,
-            'Object': inspect
-        }[ R.type ( message ) ] || console.log )( message );
+    if (debug) {
+        inspect = require('eyes').inspector({maxLength: 0});
+        ({'Array': inspect, 'Object': inspect}[R.type(message)] || console.log)(message);
     }
 };
 
-var wrapKinesisFunction = R.curry ( function ( kinesis, func, parms ) {
-    log ( 'wrapKinesisFunction' );
+var wrapKinesisFunction = R.curry(function(kinesis, func, params) {
+    log('wrapKinesisFunction');
 
-    return hl.wrapCallback ( function ( parms, callBack ) {
-        kinesis[func] ( parms, callBack );
-    } )( parms );
-} );
+    return hl.wrapCallback(function (params, callBack) {
+        kinesis[func](params, callBack);
+    })(params);
+});
 
-var getNextIteration = function ( kinesis, nextShardIterator, timeout ) {
-    log ( 'getNextIteration' );
-    log ( 'timeout: ' + timeout );
+var initialStream = hl();
 
-    return wrapKinesisFunction ( kinesis, 'getRecords', { ShardIterator: nextShardIterator, Limit: 10000 } )
-        .errors ( function ( error, push ) {
-            log ( 'error' );
+var fetchData = hl(initialStream)
+    .flatMap(function(shardIterator) {
+        return hl(function(push, next) {
+            K.getRecords({ShardIterator: shardIterator}, function(err, result) {
+                push(err, result);
+                push(null, hl.nil);
+            })
+        })
+        .errors(function(error, push) {
+            log('error');
 
-            if ( error.code !== 'ProvisionedThroughputExceededException' ) {
-                log ( 'It\'s a real error' );
-                return push ( error );
+            if (error.code !== 'ProvisionedThroughputExceededException') {
+                log('It\'s a real error');
+                return push(error);
             }
 
-            log ( 'It\'s a throughput error' );
+            log('It\'s a throughput error');
 
-            return push ( null, { NextShardIterator: nextShardIterator, Records: [] } );
-        } )
-        .flatMap ( function ( result ) {
-            log ( 'MillisBehindLatest: ' + result.MillisBehindLatest );
+            return push(null, {NextShardIterator: nextShardIterator, Records: []});
+        })
+    })
+    .map(function(data) {
+        initialStream.write(data.NextShardIterator);
+        return data.Records;
+    })
+    .compact()
 
-            return hl ( function ( push, next ) {
-                if ( R.isEmpty ( result.Records ) ) {
-                    log ( 'No Records' );
-                    return setTimeout ( function () {
-                        next ( getNextIteration ( kinesis, result.NextShardIterator, R.min ( [ timeout + R.min ( timeouts ), R.max ( timeouts ) ] ) ) );
-                    }, timeout );
-                }
+module.exports = R.curry(function(awsConfig) {
+    var kinesis = new aws.Kinesis(R.pick(['accessKeyId', 'secretAccessKey', 'region'], awsConfig));
+    var kinesisConfig = R.pick(['StreamName', 'ShardIteratorType'], awsConfig);
 
-                log ( 'Has Records' );
-                log ( 'Number of Records: ' + R.length ( result.Records ) );
-
-                R.forEach ( R.curry ( push )( null, R.__ ), result.Records );
-
-                setTimeout ( function () {
-                    next ( getNextIteration ( kinesis, result.NextShardIterator, R.max ( [ timeout - R.min ( timeouts ), R.min ( timeouts ) ] ) ) );
-                }, R.min ( timeouts ) );
-            } );
-        } );
-};
-
-module.exports = R.curry ( function ( awsConfig ) {
-    var kinesis = new aws.Kinesis ( R.pick ( [ 'accessKeyId', 'secretAccessKey', 'region' ], awsConfig ) );
-    var kinesisConfig = R.pick ( [ 'StreamName', 'ShardIteratorType' ], awsConfig );
-    
     debug = awsConfig.debug || false;
 
-    return wrapKinesisFunction ( kinesis, 'describeStream', R.omit ( [ 'ShardIteratorType' ], kinesisConfig ) )
-        .pluck ( 'StreamDescription' )
-        .pluck ( 'Shards' )
-        .map ( R.pluck ( 'ShardId' ) )
-        .flatMap ( function ( shardIdArray ) {
-            return hl ( shardIdArray )
-                .map ( R.createMapEntry ( 'ShardId' ) )
-                .map ( R.merge ( kinesisConfig ) )
-                .map ( function ( shardConfig ) {
-                    return wrapKinesisFunction ( kinesis, 'getShardIterator', shardConfig )
-                        .flatMap ( function ( result ) {
-                            return getNextIteration ( kinesis, result.ShardIterator, R.min ( timeouts ) );
-                        } );
-                } )
-                .parallel ( R.length ( shardIdArray ) );
-        } )
-        .map ( function ( record ) {
-            return record.Data.toString ();
-        } )
-        .flatMap ( function ( record ) {
-            return hl.wrapCallback ( function ( record, callBack ) {
-                try {
-                    return callBack ( null, JSON.parse ( record ) );
-                } catch ( error ) {
-                    return callBack ( error );
-                }
-            } )( record );
-        } );
-} );
+    return wrapKinesisFunction(kinesis, 'describeStream', R.omit(['ShardIteratorType'], kinesisConfig))
+        .pluck('StreamDescription')
+        .pluck('Shards')
+        .map(R.pluck('ShardId'))
+        .flatMap(function(shardIdArray) {
+            return hl(shardIdArray)
+                .map(R.createMapEntry('ShardId'))
+                .map(R.merge(kinesisConfig))
+                .flatMap(function(shardConfig) {
+                    return wrapKinesisFunction(kinesis, 'getShardIterator', shardConfig)
+                        .flatMap(function(result) {
+                            initialStream.write(result.ShardIterator);
+                            return fetchData;
+                        });
+                })
+                .sequence()
+                .map(function(record) {
+                    return record.Data.toString();
+                })
+        })
+});
